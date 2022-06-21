f5d03f700c847c58bf075cf38a91201d60d29dd0
benjaminluan <benjaminluan1@gmail.com>
2022-06-21 15:06:23 -0400

    First

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
+    // BEM
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          {/* HOME  SEARCH BUTTON */}
+          <Route path="/" element={<Home/>} />
+          {/* Search Page (Results)  */}
+          <Route path="/search" />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "../components/Search.css";
+import SearchIcon from "@mui/icons-material/Search";
+import MicIcon from "@mui/icons-material/Mic";
+
+const Search = () => {
+    return (
+        <div className='search'>
+            <div className="search__input">
+                <SearchIcon className="search__input-icon"/>
+                <input/>
+                <MicIcon />
+            </div> 
+        </div>
+    );
+}
+
+export default Search;
